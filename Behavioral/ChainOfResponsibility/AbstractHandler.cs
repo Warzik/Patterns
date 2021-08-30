51132ad3fcefbca04595779669390341b37ece58
NAZAR OŻGA <nozg00@me.pl>
2021-08-30 10:26:10 +0200

    add Chain of Responsibility

---
+++ Behavioral/ChainOfResponsibility/AbstractHandler.cs
@@ -0,0 +1,21 @@
+﻿using ChainOfResponsibility.Abstraction;
+
+namespace ChainOfResponsibility
+{
+    internal abstract class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public IHandler SetNext(IHandler handler)
+        {
+            _nextHandler = handler;
+
+            return handler;
+        }
+
+        public virtual object Handle(object request)
+        {
+            return _nextHandler?.Handle(request);
+        }
+    }
+}
---
+++ Behavioral/ChainOfResponsibility/Program.cs
@@ -0,0 +1,24 @@
+﻿using ChainOfResponsibility.Handlers;
+using System;
+
+namespace ChainOfResponsibility
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var monkey = new MonkeyHandler();
+            var squirrel = new SquirrelHandler();
+            var dog = new DogHandler();
+
+            monkey.SetNext(squirrel).SetNext(dog);
+
+            Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+            Client.ClientCode(monkey);
+            Console.WriteLine();
+
+            Console.WriteLine("Subchain: Squirrel > Dog\n");
+            Client.ClientCode(squirrel);
+        }
+    }
+}
